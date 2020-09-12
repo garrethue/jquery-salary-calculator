7d3ebcc2f760f59f3f03383199572fef6b60d85d
Garret Larson <garret.hs.larson@gmail.com>
2020-09-12 16:46:01 -0500

    Added edge case functionality. Last commit for the day.

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,21 @@
+//need to create ids for delete button
 $(document).ready(() => {
   $("#employee-form").on("submit", (e) => {
     e.preventDefault();
-    //append employee data to table
-    $("#employee-table-body").append(`<tr>
+
+    //if nothing is passed, alert the user
+    if (
+      //if any inputs are blank
+      $("#employee-ID").val() === "" ||
+      $("#employee-first-name").val() === "" ||
+      $("#employee-last-name").val() === "" ||
+      $("#employee-title").val() === "" ||
+      $("##employee-salary").val() === ""
+    ) {
+      alert("Error: please provide input for all fields!");
+    } else {
+      //append employee data to table
+      $("#employee-table-body").append(`<tr>
         <td>${$("#employee-ID").val()}</td>
         <td>${$("#employee-first-name").val()}</td>
         <td>${$("#employee-last-name").val()}</td>
@@ -10,5 +23,7 @@ $(document).ready(() => {
         <td>${$("#employee-salary").val()}</td>
         <td>${"<button class='btn btn-danger'>Delete</button>"}</td>
     </tr>`);
+    }
+    $('input[type="text"]').val(""); //clear inputs when formsubmitted
   });
 });
