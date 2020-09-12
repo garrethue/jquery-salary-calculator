e607f200fb679367576c02a9ee9478d64543fe70
Garret Larson <garret.hs.larson@gmail.com>
2020-09-12 16:29:08 -0500

    Add appending functionality to table. Next add functionality to delete button. Also include functionality for edge cases and total salary.

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,14 @@
 $(document).ready(() => {
-  //$("body").append("hello world!!!!");
+  $("#employee-form").on("submit", (e) => {
+    e.preventDefault();
+    //append employee data to table
+    $("#employee-table-body").append(`<tr>
+        <td>${$("#employee-ID").val()}</td>
+        <td>${$("#employee-first-name").val()}</td>
+        <td>${$("#employee-last-name").val()}</td>
+        <td>${$("#employee-title").val()}</td>
+        <td>${$("#employee-salary").val()}</td>
+        <td>${"<button class='btn btn-danger'>Delete</button>"}</td>
+    </tr>`);
+  });
 });
