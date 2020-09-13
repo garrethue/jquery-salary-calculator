aa2a97fc28f8a612a164b929ecca79350a0e620e
Garret Larson <garret.hs.larson@gmail.com>
2020-09-13 14:37:13 -0500

    Added comma functionality.

--- scripts/client.js
+++ scripts/client.js
@@ -1,17 +1,15 @@
-//TODO: If the total monthly cost exceeds $20,000, add a red background color to the total monthly cost.
-//TODO: package onsubmit callback
+//TODO: Handle wrong data type inputs!!!!
 //TODO: What if I submit a person, then resubmit that person? USE the COMPANY'S STR ID TO HANDLE THIS LOGIC
-//TODO: Handle wrong data type inputs
-//TODO: add commas to numbers on UI
 
-let globalEmployeePrimaryKeyID = 0; //this allows for each employee added to be uniquely identified
 const objEmployees = {}; //this object should be constant so we don't lose valuable data
 let globalTotalCosts = 0;
+let globalEmployeePrimaryKeyID = 0; //this allows for each employee added to be uniquely identified
 
 //need to create ids for delete button
 $(document).ready(() => {
   //onSubmit function
   $("#employee-form").on("submit", (e) => {
+    //prevent page from refreshing
     e.preventDefault();
     //if nothing is passed, alert the user
     if (
@@ -24,7 +22,6 @@ $(document).ready(() => {
     ) {
       alert("Error: please provide input for all fields!");
     } else {
-      //add employee to object of employees
       addEmployee(
         Number($("#employee-ID").val()),
         $("#employee-first-name").val(),
@@ -41,27 +38,39 @@ $(document).ready(() => {
         <td>${$("#employee-first-name").val()}</td>
         <td>${$("#employee-last-name").val()}</td>
         <td>${$("#employee-title").val()}</td>
-        <td>${$("#employee-salary").val()}</td>
+        <td>$${Number($("#employee-salary").val()).toLocaleString()}</td>
         <td>${`<button class='btn btn-danger delete-button' id=${globalEmployeePrimaryKeyID++}>Delete</button>`}</td> 
     </tr>`);
 
-      //update total monthly costs
-      $("#total-monthly-cost").text(`Total Monthly: $${getMonthlyCosts()}`);
+      //update total monthly costs on the DOM
+      $("#total-monthly-cost").text(
+        `Total Monthly: $${getMonthlyCosts().toLocaleString()}`
+      );
+      if (isOverBudget()) {
+        //add class
+        $("#total-monthly-cost").addClass("alert alert-danger");
+      }
     }
     //clear inputs when form submitted
     $('input[type="text"]').val("");
-  });
+  }); //end of on-submit event
 
   //filter click events to only of the class, .delete-button
   $("body").on("click", ".delete-button", (e) => {
     inactivateEmployee(Number(e.target.id), objEmployees);
     $(e.target).closest("tr").remove();
-    //update Total Monthly on the DOM:
-    $("#total-monthly-cost").text(`Total Monthly: $${getMonthlyCosts()}`);
-  });
-});
 
-//function to add an employee to object of employees
+    //update total monthly on the DOM
+    $("#total-monthly-cost").text(
+      `Total Monthly: $${getMonthlyCosts().toLocaleString()}`
+    );
+    if (!isOverBudget()) {
+      $("#total-monthly-cost").removeClass("alert alert-danger");
+    }
+  }); //end of delete button on-click event
+}); //end of document.ready()
+
+//function to add an employee to object of employees with a unique ID
 //this implementation was utilized because order is unneccessary
 //fast access/insertion/removal is needed
 const addEmployee = (
@@ -72,7 +81,6 @@ const addEmployee = (
   pnumSalary,
   pobjEmployees
 ) => {
-  //insert employee into object of employees with unique ID
   pobjEmployees[globalEmployeePrimaryKeyID] = {
     numEmployeeID: pnumEmployeeID,
     strFirstName: pstrFirstName,
@@ -94,4 +102,9 @@ const inactivateEmployee = (pnumEmployeePrimaryKey, pobjEmployees) => {
 //use global globalTotalCosts to get the montly costs
 const getMonthlyCosts = () => {
   return Math.round((globalTotalCosts / 12) * 100) / 100;
+};
+
+// check if company is over budget
+const isOverBudget = () => {
+  return globalTotalCosts > 240000; // 20,000 = total / 12
 };
